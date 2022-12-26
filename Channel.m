afa22740d3b92ea37b33b0da929f0f2df23e8707
Mohamed Hassan <99367479+MohamedFola@users.noreply.github.com>
2022-12-26 03:50:45 +0200

    Using the implemented AWGN function

--- Channel.m
+++ Channel.m
@@ -3,38 +3,46 @@ function [RX_Symbols1 , RX_Symbols2] = Channel(TX_Symbols , SNR , channelmode , 
 %
 % Inputs:
 %       TX_Symbols : the transmitted signal (Vector)
-%       SNR : signal to noise ration for the AWGN (Scalar)
-%       mode : mode of the channel (1-2-3)
+%       SNR        : signal to noise ration for the AWGN (Scalar)
+%       mode       : mode of the channel (1-2-3)
 %       no_outputs : 1 or 2 weather its SISO or SIMO
 %
 % Outputs:
 %        RX_Symbols1 , RX_Symbols2 : the recieved signal due to the 2 channels
 
-  L = 50;  % Fading channel delay
+L = 50;  % Fading channel delay
 
 switch channelmode
-    
+  
+  % Case 1 --> AWGN 
   case 1
-    % case 1 AWGN only
-    RX_Symbols1 = awgn(TX_Symbols, SNR);  % adding noise to the signal
+    % Adding Noise to the Signal
+    RX_Symbols1 = addAWGN( TX_Symbols, SNR );
+    
+    % Check for SISO or SIMO
     if no_outputs == 2
-      RX_Symbols2 = awgn(TX_Symbols, SNR); % adding noise to the signal
+      % Adding Noise to the Signal
+      RX_Symbols2 = addAWGN( TX_Symbols, SNR ); 
     else
+      % Set the second RX symbol to 0 as we have single output
       RX_Symbols2 = 0;
     end
     
-  case 2
-    % case 2 AWGN with deterministic fading channel realization
-    h1 = 1 / sqrt(2 * L) * (randn(1, L) + 1i * randn(1, L)); % calculating channel gain
+  % Case 2 --> AWGN with deterministic fading channel realization
+  case 2 
+    h1              = 1 / sqrt(2 * L) * (randn(1, L) + 1i * randn(1, L)); % calculating channel gain
+    
     TX_Symbols_conv = conv(TX_Symbols, h1);
-    RX_Symbols1 = awgn(TX_Symbols_conv, SNR); % adding noise and channel gain to the signal
-    if no_outputs == 2
-      h2 = 1 / sqrt(2 * L) * (randn(1, L) + 1i * randn(1, L)); % calculating channel gain
+    
+    RX_Symbols1     = awgn(TX_Symbols_conv, SNR); % adding noise and channel gain to the signal
+    
+    if no_outputs == 2  
+      h2              = 1 / sqrt(2 * L) * (randn(1, L) + 1i * randn(1, L)); % calculating channel gain
       TX_Symbols_conv = conv(TX_Symbols, h2);
-      RX_Symbols2 = awgn(TX_Symbols_conv, SNR); % adding noise and channel gain to the signal
+      RX_Symbols2     = awgn(TX_Symbols_conv, SNR); % adding noise and channel gain to the signal
+      
     else
       RX_Symbols2 = 0;
     end
-%   otherwise
-%     % code to be executed if channelmode is not 1 or 2
+
 end
